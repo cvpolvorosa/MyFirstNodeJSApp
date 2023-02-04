4d43f9c1b86de7b88b53efb5940fe0cc3ff8fa33
cvpolvorosa <cvpolvorosa@gmail.com>
2023-02-04 18:30:17 +0800

    Added redirection of requests

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     const url = req.url; //parse url
+    const method = req.method; //parse http method
     if(url === '/'){
         res.write('<html>')
         res.write('<head><title>Enter your message</title></head>')
@@ -9,13 +11,18 @@ const server = http.createServer((req,res) => {
         res.write('</html>')
         return res.end();
     }
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>My First NodeJS App</title></head>')
-    res.write('<body><h1>Hello World!</h1></body>')
-    res.write('</html>')
-    res.end();
+    if (url === '/message' && method === 'POST'){
+        fs.writeFileSync("message.txt","Test Message")
+        res.statusCode = 302; //status code 302 for redirection
+        res.setHeader('Location', '/'); //Location is a default header accepted by the browser; '/' uses the host
+        return res.end();
+    }
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>My First NodeJS App</title></head>')
+        res.write('<body><h1>Hello World!</h1></body>')
+        res.write('</html>')
+        res.end();
 });
 
 server.listen(3000);
