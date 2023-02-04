638fa2cf075df796ccbcb25b06b383c498e40862
cvpolvorosa <cvpolvorosa@gmail.com>
2023-02-04 18:45:05 +0800

    Added listeners to get and store data

--- app.js
+++ app.js
@@ -12,7 +12,17 @@ const server = http.createServer((req,res) => {
         return res.end();
     }
     if (url === '/message' && method === 'POST'){
-        fs.writeFileSync("message.txt","Test Message")
+        const body = [];
+        req.on('data', (chunk) => { //on allows us to listen to certain events ex. data, close, end, error ...
+            body.push(chunk);
+            console.log(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1]; //split is used because the expected output is "message=<user's message>"
+            fs.writeFileSync("message.txt", message)
+        });
         res.statusCode = 302; //status code 302 for redirection
         res.setHeader('Location', '/'); //Location is a default header accepted by the browser; '/' uses the host
         return res.end();
